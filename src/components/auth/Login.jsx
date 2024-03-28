8f3377ab423523323af0b86a96b680bd8061ec72
HARSHTANDEL25 <140244670+HARSHTANDEL25@users.noreply.github.com>
2024-03-28 15:59:11 +0530

    commit

--- src/components/auth/Login.jsx
+++ src/components/auth/Login.jsx
@@ -15,7 +15,7 @@ const Login = () => {
     const handleSubmit = async (e) => {
         e.preventDefault();
         try {
-            const url = "https://sneaker-hub.vercel.app/login/api/auth";
+            const url = "http://localhost:8080/api/auth";
             const { data: res } = await axios.post(url, data);
             localStorage.setItem("token", res.data);
             window.location = "/";
