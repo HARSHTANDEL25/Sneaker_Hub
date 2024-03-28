3030e4e42a3ec7fbf32977110f77f5bd4501f3f0
HARSHTANDEL25 <140244670+HARSHTANDEL25@users.noreply.github.com>
2024-03-28 23:08:11 +0530

    commit

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -3,12 +3,14 @@ const { User } = require("../models/user");
 const bcrypt = require("bcrypt");
 const Joi = require("joi");
 
+
 router.post("/", async (req, res) => {
 	try {
 		const { error } = validate(req.body);
 		if (error)
 			return res.status(400).send({ message: error.details[0].message });
 
+			
 		const user = await User.findOne({ email: req.body.email });
 		if (!user)
 			return res.status(401).send({ message: "Invalid Email or Password" });
